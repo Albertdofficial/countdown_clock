b236a7aae0fa1f0eea01ce9245515eb4e1aeaee9
unknown <abanguralbert@gmail.com>
2022-03-26 03:18:55 +0300

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { useState } from "react";
+// import Background from '../images/background_image.jpg';
+
+function App() {
+  let [days, setDays] = useState(0);
+  let [hours, setHours] = useState(0);
+  let [minutes, setMinutes] = useState(0);
+  let [seconds, setSeconds] = useState(0);
+
+  setInterval(function () {
+    const yearNow = new Date().getFullYear();
+    const futureDate = new Date(yearNow, 3, 11); //months starts counting from zero
+    const dateNow = new Date();
+
+    console.log(futureDate);
+
+    seconds = Math.floor((futureDate - dateNow) / 1000);
+    minutes = Math.floor(seconds / 60);
+    hours = Math.floor(minutes / 60);
+    days = Math.floor(hours / 24);
+    hours = hours - days * 24; // hours remained
+    minutes = minutes - days * 24 * 60 - hours * 60;
+    seconds = seconds - days * 24 * 3600 - hours * 3600 - minutes * 60;
+
+    setDays(days);
+    setHours(hours);
+    setMinutes(minutes);
+    setSeconds(seconds);
+  }, 1000);
+
+  return (
+    <div className="App">
+      <h1>
+        {days} days : {hours} hours : {minutes} minutes : {seconds} seconds
+        <p>Before Midterm Exams, Spring Semester 2022</p>
+      </h1>
+
+      <footer>
+        <p className="developer">Developed by Albert David Bangura</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
